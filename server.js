619b54541a46c2f94f749fc60702ee9a8710e332
anshul0888 <anshulsharma0888@gmail.com>
2017-08-02 13:10:51 +0530

    tokenAuth

---
+++ server.js
@@ -0,0 +1,62 @@
+// =================================================================
+// get the packages we need ========================================
+// =================================================================
+var express 	= require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var User   = require('./app/models/user'); // get our mongoose model
+var apiRoutes = require('./routes/routes');
+// =================================================================
+// configuration ===================================================
+// =================================================================
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+mongoose.connect(config.database); // connect to database
+app.set('superSecret', config.secret); // secret variable
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =================================================================
+// routes ==========================================================
+// =================================================================
+app.get('/setup', function(req, res) {
+
+	// create a sample user
+	var nick = new User({ 
+		name: 'Nick', 
+		password: 'password',
+		admin: true 
+	});
+	nick.save(function(err) {
+		if (err) throw err;
+
+		console.log('User saved successfully');
+		res.json({ success: true });
+	});
+});
+
+// basic route (http://localhost:8080)
+app.get('/', function(req, res) {
+	res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+
+// ---------------------------------------------------------
+// get an instance of the router for api routes
+// ---------------------------------------------------------
+
+app.use('/api', apiRoutes);
+
+// =================================================================
+// start the server ================================================
+// =================================================================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
